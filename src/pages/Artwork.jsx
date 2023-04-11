eb35f63f8a7ccef0222af33a49111a79879cc9b2
futuresarcade <futuresarcade@gmail.com>
2023-04-11 10:40:57 -0400

    committed cchanges to new gallery

--- src/pages/Artwork.jsx
+++ src/pages/Artwork.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 
 export default function Artwork() {
     const [artwork, setArtwork] = useState([])
@@ -13,8 +13,20 @@ export default function Artwork() {
                 const select = data.find(item => item.id === id)
                 setArtwork(select)
             })
-    }, [])
-    console.log(artwork)
+    }, [id])
+    const handleNext = () => {
+        // find the next artwork's id
+        const nextId = parseInt(id) + 1;
+        // navigate to the next artwork
+        window.location.href = `/${nextId}`;
+    };
+
+    const handlePrev = () => {
+        // find the previous artwork's id
+        const prevId = parseInt(id) - 1;
+        // navigate to the previous artwork
+        window.location.href = `/${prevId}`;
+    };
 
     return (
         <div>
@@ -22,8 +34,14 @@ export default function Artwork() {
                 <>
                     <img src={artwork.image} alt={artwork.name} className="fullImage" />
                     <h2>{artwork.name}</h2>
+                    <div className="nav-buttons">
+                        <button onClick={handlePrev} disabled={id === "1"}><span className="arrow arrow-left"></span>{"<"}</button>
+                        <button onClick={handleNext} disabled={id === "18"}>{">"}<span className="arrow arrow-right"></span></button>
+                    </div>
                 </>
             )}
+
+
         </div>
     );
 
