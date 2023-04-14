bd553bd99dd62d06e8d1f24a3b60539c8b098dec
futuresarcade <futuresarcade@gmail.com>
2023-04-13 22:31:28 -0400

    fixed bio no ul

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -6,15 +6,14 @@ export default function Bio() {
             <p>
                 Self - taught artist, he went to Rome 1960 where he spent a few years,
                 going from there to New York where he opened a studio, in 1970 he
-                moved to Palm Beach, Florida. Selected Exhibitions and collections include:
-            </p>
-                    <ul>
-                    <li>The OEA Permanent Collection of Art, Washington D.C.,</li>
-                    <li>The Mint Museum of Art, Charlotte, North Carolina.</li>
-                    <li>The Society of the Four Arts, Palm Beach, Florida,</li>
-                    <li>West Palm Beach International Airport, Palm Beach, Florida.</li>
-                    </ul>
+                moved to Palm Beach, Florida. Selected Exhibitions and collections include:<br />
+                The OEA Permanent Collection of Art, Washington D.C.<br />
+                The Mint Museum of Art, Charlotte, North Carolina.<br />
+                The Society of the Four Arts, Palm Beach, Florida,<br />
+                West Palm Beach International Airport, Palm Beach, Florida.<br />
+                    
                 <hr></hr>
+            </p>
         </div>
     )
 }
